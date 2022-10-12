1b4d85b0ebb793d5693a96357b8fb1530e8ae237
Ali <dev@macs-MacBook-Pro.local>
2022-10-12 19:17:15 +0500

    Scraped the different features.

--- mobilescraper/items.py
+++ mobilescraper/items.py
@@ -8,5 +8,7 @@ import scrapy
 
 class MobilescraperItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    name = scrapy.Field()
+    url = scrapy.Field()
+    price = scrapy.Field()
+
--- mobilescraper/spiders/mobilescraper.py
+++ mobilescraper/spiders/mobilescraper.py
@@ -1,4 +1,6 @@
 import scrapy
+import csv
+import pandas as pd
 
 
 class MobileScraper(scrapy.Spider):
@@ -7,10 +9,27 @@ class MobileScraper(scrapy.Spider):
                   'https://www.whatmobile.com.pk/Huawei_Mobiles_Prices',
                   'https://www.whatmobile.com.pk/Vivo_Mobiles_Prices']
 
+    mobile_dict = dict()
+
     def parse(self, response):
         for products in response.css('div.item'):
-            yield {
-                'name': products.css('a').attrib['href'].replace('/', ''),
-                'price': products.css('span.PriceFont::text').get().replace('\n', '').replace('Rs. ', ''),
-                'link': products.css('a').attrib['href']
-            }
+
+            link = 'https://www.whatmobile.com.pk/' + \
+                   products.css('a').attrib['href']
+
+            yield scrapy.Request(
+                link, self.parse_products, dont_filter=False)
+
+    def parse_products(self, response):
+        for block in response.css('div#centerContainer'):
+            self.mobile_dict['Name'] = block.css('h1.hdng3::text').get()
+            self.mobile_dict['Price'] = block.css(
+                'span.hdng::text').get().replace('\nRs. ', '')
+
+        for table in response.css('table.specs'):
+            features = table.css(
+                'td.specs-value::text').getall()
+            for i in range(0, len(features)):
+                self.mobile_dict[f'Feature{i}'] = features[i].replace(
+                    '\xa0\n', '')
+            yield self.mobile_dict
